d56bd7a6b9a987cbe858f468a00d9baf0a498f8a
yasin <yasinmiapolash@gmail.com>
2024-05-26 12:42:55 +0600

    storage data by get storage

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class HomeController extends GetxController {
   var isDark = false;
 
-  void changeTheme(state) {
-    if (state == true) {
-      isDark = true;
-      Get.changeTheme(ThemeData.dark());
+  final saveModeType = GetStorage();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _loadTheme();
+  }
+
+  void _loadTheme() {
+    if (saveModeType.hasData('isDark')) {
+      isDark = saveModeType.read('isDark');
     } else {
-      isDark = false;
-      Get.changeTheme(ThemeData.light());
+      var brightness = WidgetsBinding.instance.window.platformBrightness;
+      isDark = (brightness == Brightness.dark);
     }
+    Get.changeThemeMode(isDark ? ThemeMode.dark : ThemeMode.light);
+    update();
+  }
+
+  void changeTheme(bool state) {
+    isDark = state;
+    Get.changeThemeMode(isDark ? ThemeMode.dark : ThemeMode.light);
+    saveModeType.write('isDark', isDark);
     update();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
-import 'package:darktheme/app/modules/color.dart';
+import 'package:darktheme/app/modules/home/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/core/bindings/application_bindings.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialBinding: ApplicationBindings(),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      theme: ThemeData(primaryColor: colorPrimary),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  final HomeController homeController = Get.put(HomeController());
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeController>(
+      builder: (homeController) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialBinding: ApplicationBindings(),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode: homeController.isDark ? ThemeMode.dark : ThemeMode.light,
+        );
+      },
+    );
+  }
 }
